9413b4145b99a06b22519c68b85375d86e28667c
maatheusgois-dd <matheus.gois@doordash.com>
2025-04-03 22:49:43 -0300

    Initial Commit

---
+++ AccessoryDetector/AccessoryDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessoryDetectorApp.swift
+//  AccessoryDetector
+//
+//  Created by Matheus Gois on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AccessoryDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessoryDetectorTests/AccessoryDetectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  AccessoryDetectorTests.swift
+//  AccessoryDetectorTests
+//
+//  Created by Matheus Gois on 03/04/25.
+//
+
+import Testing
+@testable import AccessoryDetector
+
+struct AccessoryDetectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
